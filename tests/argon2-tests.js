2155898f5477f8d2fc02d979137b2dcfb98bf5f8
Ivan <SolFly86@gmail.com>
2020-03-27 00:07:09 +0200

    Update argon2-tests.js

--- tests/argon2-tests.js
+++ tests/argon2-tests.js
@@ -7,7 +7,7 @@ console.log('Argon2 Tests')
 
 const xmrigdata = new Buffer('0100fb8e8ac805899323371bb790db19218afd8db8e3755d8b90f39b3d5506a9abce4fa912244500000000ee8146d49fa93ee724deb57d12cbc6c6f3b924d946127c7a97418f9348828f0f02', 'hex')
 
-const chukwa = new Buffer('c0dad0eeb9c52e92a1c3aa5b76a3cb90bd7376c28dce191ceeb1096e3a390d2e', 'hex')
+const chukwa = new Buffer('87766c65eb2305545bb8bb5b6dddee060f9c43051de3a94c554223422549d0f5', 'hex')
 
 const chukwaData = multiHashing['chukwa'](xmrigdata)
 
